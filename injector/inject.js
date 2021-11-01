e99a9ff2bce99e4d40ead194f3ca8385dd8c565b
Burrito <37681398+burritosoftware@users.noreply.github.com>
2021-10-31 20:49:58 -0700

    Add new injector

---
+++ injector/inject.js
@@ -0,0 +1,24 @@
+const regedit = require('regedit');
+const { join, sep } = require('path');
+const fs = require("fs");
+
+test = regedit.list('HKLM\\SOFTWARE\\WOW6432Node\\Oculus VR, LLC\\Oculus').on('data', function(entry) {
+    var oculusPath = join(entry.data.values.Base.value, '/Support/oculus-client/resources');
+    console.log('Found Oculus installation at:\n' + oculusPath + '\n');
+
+    if(fs.existsSync(join(oculusPath, '/app'))) {
+        console.log('OcuTweaks is already installed. Run "npm run uninject" to uninstall OcuTweaks.');
+    } else {
+        try {
+            oculusAppAsar = join(oculusPath, '/app');
+            fs.mkdirSync(oculusAppAsar);
+            const patcherPath = join(__dirname, "../patcher/patch.js").replace(RegExp(sep.repeat(2), "g"), "/");
+            fs.writeFileSync(join(oculusAppAsar, "index.js"), `require("${patcherPath}");`);
+            fs.writeFileSync(join(oculusAppAsar, "package.json"), JSON.stringify({ name: "Oculus", main: "index.js", version: "0.0.0" }));
+            console.log('OcuTweaks has been injected successfully! Restart the Oculus app for changes to take effect.');
+        } catch (e) {
+            console.log(e + '\n');
+            console.log('Failed to inject into Oculus app.\nPlease make sure to run "npm run inject" in an Administrator command prompt.');
+        }
+    }
+});
